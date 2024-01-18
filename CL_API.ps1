b072962fbc1a68465b9bfd36dc802e83e70e4383
zhanggy-31 <80073009+zhanggy-31@users.noreply.github.com>
2024-01-18 10:12:09 +0800

    Add files via upload

---
+++ CL_API.ps1
@@ -0,0 +1,80 @@
+﻿# Replace with your Workspace ID
+$CustomerId = "d22cdb82-f768-4a86-87b1-749c13bd592d"  
+
+# Replace with your Primary Key
+$SharedKey = "dlSUJFp0lb9CgOVwJPuh3QacBwXJ0x3ANhDX98FCNSc7+cit+rVP5a/FZpmAq5V2pAcG8MVmTdY61f4oaEXqtA=="
+
+# Specify the name of the record type that you'll be creating
+$LogType = "customlogAPItest_CL"
+
+# Optional name of a field that includes the timestamp for the data. If the time field is not specified, Azure Monitor assumes the time is the message ingestion time
+$TimeStampField = ""
+
+
+# Create two records with the same set of properties to create
+$json = @"
+[
+{
+            'CreatedBy': 'victor',
+            'CreatedTime': '$currentTime',
+            'Addline1': 'testline1',
+            'Addline2': 'testline2'
+},
+{
+            'CreatedBy': 'victor',
+            'CreatedTime': '$currentTime',
+            'Addline1': 'testline1',
+            'Addline2': 'testline2'
+}
+]
+"@
+
+# Create the function to create the authorization signature
+Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
+{
+    $xHeaders = "x-ms-date:" + $date
+    $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+
+    $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+    $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+    $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+    $sha256.Key = $keyBytes
+    $calculatedHash = $sha256.ComputeHash($bytesToHash)
+    $encodedHash = [Convert]::ToBase64String($calculatedHash)
+    $authorization = 'SharedKey {0}:{1}' -f $customerId,$encodedHash
+    return $authorization
+}
+
+# Create the function to create and post the request
+Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType)
+{
+    $method = "POST"
+    $contentType = "application/json"
+    $resource = "/api/logs"
+    $rfc1123date = [DateTime]::UtcNow.ToString("r")
+    $contentLength = $body.Length
+    $signature = Build-Signature `
+        -customerId $customerId `
+        -sharedKey $sharedKey `
+        -date $rfc1123date `
+        -contentLength $contentLength `
+        -method $method `
+        -contentType $contentType `
+        -resource $resource
+    $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    $headers = @{
+        "Authorization" = $signature;
+        "Log-Type" = $logType;
+        "x-ms-date" = $rfc1123date;
+        "time-generated-field" = $TimeStampField;
+    }
+
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    return $response
+
+}
+
+# Submit the data to the API endpoint
+Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
